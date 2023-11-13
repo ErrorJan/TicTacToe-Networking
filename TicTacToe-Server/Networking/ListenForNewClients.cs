c112b639aaa954a952b238e22d6b31470028ef11
ErrorJan <50844353+ErrorJan@users.noreply.github.com>
2023-11-13 09:21:12 +0100

    Server theoretically done. Testing required.

--- TicTacToe-Server/Networking/ListenForNewClients.cs
+++ TicTacToe-Server/Networking/ListenForNewClients.cs
@@ -54,6 +54,8 @@ class ListenForNewClients
             PlayerNext();
 
             await gameFinished.Task;
+
+            game.Disconnect().Wait();
         }
     }
 
@@ -61,8 +63,49 @@ class ListenForNewClients
     {
         if ( player == currentPlayer )
         {
-            
+            if ( game?.board.board[move.x][move.y] == BoardPlace.Unassigned )
+            {
+                game.UpdateBoard( move ).Wait();
+
+                bool playerWon = CheckBoard( game.board.board, move );
+
+                if ( playerWon )
+                {
+                    game.PlayerWin( player ).Wait();
+                    gameFinished?.SetResult();
+                }
+                else
+                    PlayerNext();
+            }
+            else
+            {
+                console.Error( "Player moved Illegaly!" );
+            }
         }
+    }
+
+    private bool CheckBoard( BoardPlace[][] array, BoardMove move )
+    {
+        // 0: 0 1 2
+        // 1: 0 1 2
+        // 2: 0 1 2
+
+        bool topLeft     = array[0][0] == move.place;
+        bool topRight    = array[0][2] == move.place;
+        bool bottomLeft  = array[2][0] == move.place;
+        bool bottomRight = array[2][2] == move.place;
+
+        bool middleLeft = array[1][0] == move.place;
+        bool middleRight = array[1][2] == move.place;
+        
+        bool top = array[0][1] == move.place;
+        bool middle = array[1][1] == move.place;
+        bool bottom = array[2][1] == move.place;
+        
+        return 
+        ( middle && ( topLeft && bottomRight || topRight && bottomLeft || ( top && bottom ) || ( middleRight && middleLeft ) ) ) ||
+        ( topLeft && ( ( topRight && top ) || ( bottomLeft && middleLeft ) ) ) ||
+        ( bottomRight && ( ( topRight && middleRight ) || ( bottomLeft && bottom ) ) ); 
     }
 
     private void PlayerNext()
