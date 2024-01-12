dddcae207334a789f6ca6fef08a8a4ba493ee885
Chaotyc41 <150123617+Chaotyc41@users.noreply.github.com>
2024-01-12 16:14:10 +0100

    Addition of connection
    Added part of the code needed for the connection to the server and the player controls.

--- TicTacToe UI/TicTacToe/Form1.cs
+++ TicTacToe UI/TicTacToe/Form1.cs
@@ -1,10 +1,13 @@
+using System.Net;
+using TicTacToe_Shared;
+
 namespace TicTacToe
 {
     public partial class Form1 : Form
     {
         bool Spieler1 = true, SpEnde = false;
         int SiegeSp1 = 0, SiegeSp2 = 0;
-        private Button[] but = new Button[9];
+        private Button[,] but = new Button[3,3];
         private Label[] lab = new Label[5];
         public Form1()
         {
@@ -20,29 +23,28 @@ namespace TicTacToe
             posY = 30;
 
 
-            for (int i = 0; i < 9; i++)     // 9 Buttons für das Spiel Layout
+            for (int i = 0; i < 3; i++)     // 9 Buttons für das Spiel Layout
             {
-                but[i] = new Button();
-                but[i].Width = 50;
-                but[i].Height = 50;
-                but[i].Location = new Point(posX, posY);
-                but[i].Font = new Font("Microsoft sans serif", 24, FontStyle.Bold);
-                but[i].Name = "but" + (i + 1);
-                but[i].Click += buttonsClick;
-                this.Controls.Add(but[i]);
+                for (int j = 0; j < 3; j++)
+                { 
+                    but[i,j] = new Button();
+                    but[i,j].Width = 50;
+                    but[i,j].Height = 50;
+                    but[i,j].Location = new Point(posX, posY);
+                    but[i,j].Font = new Font("Microsoft sans serif", 24, FontStyle.Bold);
+                    but[i,j].Name = $"but{i},{j}";
+                    but[i,j].Click += buttonsClick;
+                    this.Controls.Add(but[i,j]);
 
-                posX += 65;
-
-                if (i == 2 || i == 5)      // passiert nur wenn eine neue Zeile gebraucht wird
-                {
-                    posX -= 195;
-                    posY += 60;
+                    posX += 65;              
                 }
+                posX -= 195;
+                posY += 60;                            
             }
 
 
 
-            for (int i = 0; i < 5; i++)    // 5 verschiedend Labels die gebraucht sind (noch nicht alle fertig)
+            for (int i = 0; i < 5; i++)    // 5 verschieden Labels die gebraucht sind (noch nicht alle fertig)
             {                              // bin mir unsicher ob wir alle brauchen uberhaupt, ist jetzt da einfach
                 lab[i] = new Label();
                 switch (i)
@@ -67,9 +69,12 @@ namespace TicTacToe
 
         void Clearall()            // Alles auf Anfang setzen 
         {
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < 3; i++)
             {
-                but[i].Text = "";
+                for (int j = 0; j < 3; j++)
+                {
+                    but[i,j].Text = "";
+                }               
             }
 
             lab[0].Text = "Spieler 1 am Zug";
@@ -82,31 +87,38 @@ namespace TicTacToe
         private void buttonsClick(object sender, EventArgs e)       
         {
             Button aktBut = (Button)sender;
-            if (aktBut.Text == "")
+            if (aktBut.Text == "" && Connection.currentTurn )
             {
-                if (Spieler1)
-                {
-                    aktBut.Text = "X";
-                    lab[0].Text = "Spieler 2 am Zug";
-                    Spieler1 = false;
-                }
-                else
-                {
-                    lab[0].Text = "Spieler 1 am Zug";
-                    aktBut.Text = "O";
-                    Spieler1 = true;
-                }
-
+                Connection.SendAction(Convert.ToInt32(aktBut.Name[3])-48, Convert.ToInt32(aktBut.Name[5])-48);              
             }
-
             return;
         }
+        private void PlayerEvent(BoardMove move)
+        {
+            but[move.x,move.y].Text = move.place.ToString();
+        }
 
-        // Funktion für den Button des neues Spieles
-        // wird am Ende vielleicht auch nicht gebraucht, jetzt für Probe da
+        private void PlayerTurnEvent(PlayerData player)
+        {
+            if (Connection.currentTurn)
+            {
+                lab[1].Text = player.playerName;  
+            }
+        }
+
+        private void PlayerWonEvent(byte playerID)
+        {
+
+        }
+
+        ClientConnection Connection;
         private void button1_Click(object sender, EventArgs e)   
         {                
             Clearall();
+            Connection = new("Hope", IPEndPoint.Parse($"127.0.1.2:{StaticGameInfo.GAME_PORT}"));
+            Connection.playerEvent += PlayerEvent;
+            Connection.playerTurnEvent += PlayerTurnEvent;
+            Connection.playerWonEvent += PlayerWonEvent;
         }
     }
 }
