61284f0e6f0307ae17d67add6cccd694ba489c05
DeusExAlexandria <89421194+DeusExAlexandria@users.noreply.github.com>
2024-01-16 21:03:23 +0100

    Ip Input thing
    I coded smth im helpful

--- TicTacToe UI/TicTacToe/Form1.cs
+++ TicTacToe UI/TicTacToe/Form1.cs
@@ -1,21 +1,30 @@
+using System.Globalization;
 using System.Net;
+using System.Text.RegularExpressions;
 using TicTacToe_Shared;
 
 namespace TicTacToe
 {
     public partial class Form1 : Form
     {
-        private Button[,] but = new Button[3,3];
+        private Button[,] but = new Button[3, 3];
         private Label[] lab = new Label[5];
         private string name;
+        private string Ep;
         public Form1()
         {
             InitializeComponent();
-            name=Microsoft.VisualBasic.Interaction.InputBox("Bitte geben Sie Ihren Namen ein.", "Nameauswahl", "Name");
+            name = Microsoft.VisualBasic.Interaction.InputBox("Bitte geben Sie Ihren Namen ein.", "Nameauswahl", "Name");
             erzeuge();
             Clearall();
         }
-
+        /* public Form2()
+         {
+             InitializeComponent();
+             name = Microsoft.VisualBasic.Interaction.InputBox("Bitte server", "Nameauswahl", "Name");
+             erzeuge();
+             Clearall();
+         }*/
         private void erzeuge()      // erzeugt alle buttons dynamisch
         {
             int posX, posY;
@@ -26,20 +35,20 @@ namespace TicTacToe
             for (int i = 0; i < 3; i++)     // 9 Buttons für das Spiel Layout
             {
                 for (int j = 0; j < 3; j++)
-                { 
-                    but[i,j] = new Button();
-                    but[i,j].Width = 50;
-                    but[i,j].Height = 50;
-                    but[i,j].Location = new Point(posX, posY);
-                    but[i,j].Font = new Font("Microsoft sans serif", 24, FontStyle.Bold);
-                    but[i,j].Name = $"but{i},{j}";
-                    but[i,j].Click += buttonsClick;
-                    this.Controls.Add(but[i,j]);
+                {
+                    but[i, j] = new Button();
+                    but[i, j].Width = 50;
+                    but[i, j].Height = 50;
+                    but[i, j].Location = new Point(posX, posY);
+                    but[i, j].Font = new Font("Microsoft sans serif", 24, FontStyle.Bold);
+                    but[i, j].Name = $"but{i},{j}";
+                    but[i, j].Click += buttonsClick;
+                    this.Controls.Add(but[i, j]);
 
-                    posX += 65;              
+                    posX += 65;
                 }
                 posX -= 195;
-                posY += 60;                            
+                posY += 60;
             }
 
 
@@ -65,8 +74,9 @@ namespace TicTacToe
                         lab[i].Font = new Font("Microsoft sans serif", 12, FontStyle.Bold);
                         lab[i].Name = "lab" + (i + 1);
                         lab[i].Text = "";
-                        this.Controls.Add(lab[i]); break;        // Label 2: 
-                    case 2: break;
+                        this.Controls.Add(lab[i]);
+                        break;        // Label 2: 
+                    case 2:break;
                     case 3: break;
                     case 4: break;
                 }
@@ -80,8 +90,8 @@ namespace TicTacToe
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    but[i,j].Text = "";
-                }               
+                    but[i, j].Text = "";
+                }
             }
 
             lab[0].Text = name;
@@ -90,38 +100,47 @@ namespace TicTacToe
         }
 
         // Funktion für die Buttons der Map
-        private void buttonsClick(object sender, EventArgs e)       
+        private void buttonsClick(object sender, EventArgs e)
         {
             Button aktBut = (Button)sender;
-            if (aktBut.Text == "" && Connection.currentTurn )
+            if (aktBut.Text == "" && Connection.currentTurn)
             {
-                Connection.SendAction(Convert.ToInt32(aktBut.Name[3])-48, Convert.ToInt32(aktBut.Name[5])-48);              
+                Connection.SendAction(Convert.ToInt32(aktBut.Name[3]) - 48, Convert.ToInt32(aktBut.Name[5]) - 48);
             }
             return;
         }
         private void PlayerEvent(BoardMove move)
         {
-            but[move.x,move.y].Text = move.place.ToString();
+            but[move.x, move.y].Text = move.place.ToString();
         }
 
         private void PlayerTurnEvent(PlayerData player)
-        {                      
-            lab[0].Text = player.playerName + " ist am Zug.";             
+        {
+            lab[0].Text = player.playerName + " ist am Zug.";
         }
 
-        private void PlayerWonEvent( PlayerData player )
+        private void PlayerWonEvent(PlayerData player)
         {
             lab[1].Text = player.playerName + " hat gewonnen";
         }
 
         ClientConnection Connection;
-        private void button1_Click(object sender, EventArgs e)   
-        {                
+        private void button1_Click(object sender, EventArgs e)
+        {
             Clearall();
-            Connection = new(name, IPEndPoint.Parse($"127.0.1.2:{StaticGameInfo.GAME_PORT}"));
+            Connection = new(name, IPEndPoint.Parse(Ep)); // $"127.0.1.2:{StaticGameInfo.GAME_PORT}") <--- stay here
             Connection.playerEvent += PlayerEvent;
             Connection.playerTurnEvent += PlayerTurnEvent;
             Connection.playerWonEvent += PlayerWonEvent;
         }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InitializeComponent();
+            Ep = Microsoft.VisualBasic.Interaction.InputBox("Wählen sie die Ip", "IP-Selector", "127.0.1.2");
+            Ep = Ep + ":" + StaticGameInfo.GAME_PORT;
+            erzeuge();
+            Clearall();
+        }
+
     }
 }
